3a7e9ad1a85d270c57b57a7521e92f226bf7b19c
branansmith <branansmith@gmail.com>
2025-06-23 11:17:16 -0400

    init commit + frontend

---
+++ src/components/Description.jsx
@@ -0,0 +1,5 @@
+const Description = () => (
+    <p id = "description">The feedback you've always wanted</p>
+)
+
+export default Description;
---
+++ src/components/RoastumeIcon.jsx
@@ -0,0 +1,7 @@
+const RoastumeIcon = () => (
+
+    <h2 id="title">Roastume</h2>
+
+)
+
+export default RoastumeIcon;
